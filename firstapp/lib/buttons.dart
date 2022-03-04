9e3ed273f9f7d50817c2a6480681ffece34a5f91
zabavnikCoder <jabakanchaveli@gmail.com>
2022-03-04 19:06:53 +0400

    Calculator

---
+++ firstapp/lib/buttons.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+
+class MyButton extends StatelessWidget {
+final color;
+final textColor;
+final String buttonText;
+final buttontapped;
+
+MyButton({this.color, this.textColor, required this.buttonText, this.buttontapped});
+
+@override
+Widget build(BuildContext context) {
+	return GestureDetector(
+	onTap: buttontapped,
+	child: Padding(
+		padding: const EdgeInsets.all(0.2),
+		child: ClipRRect(
+		child: Container(
+			color: color,
+			child: Center(
+			child: Text(
+				buttonText,
+				style: TextStyle(
+				color: textColor,
+				fontSize: 20,
+				fontWeight: FontWeight.bold,
+				),
+			),
+			),
+		),
+		),
+	),
+	);
+}
+}
